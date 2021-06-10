e69034e2c55bdf3902d5130a5cdb94534c42a7b0
sampsonliu <sampsonliu@tencent.com>
2021-06-10 18:55:57 +0800

    README

--- build-test.sh
+++ build-test.sh
@@ -1,9 +1,4 @@
 #/bin/sh
 set -x
-echo "cd to script dir"
-work_path=$(dirname $0)
-cd ./${work_path}
-
 sh pull-code.sh
-
 sh build.sh TEST
--- pull-code.sh
+++ pull-code.sh
@@ -1,7 +1,5 @@
 #/bin/sh
 set -x
-work_path=$(dirname $0)
-cd ./${work_path}
 git branch
 git checkout main
 git pull
