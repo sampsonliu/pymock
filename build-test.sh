a1715fef7901795617ce282768d1ca02f3d3ae2a
sampsonliu <sampsonliu@tencent.com>
2021-06-10 19:10:06 +0800

    README

--- build-test.sh
+++ build-test.sh
@@ -4,12 +4,11 @@ set -x
 echo "start deployment"
 WEB_PATH='/root/code/github.com/'
 cd $WEB_PATH/pymock
+echo "cd to script dir"
 echo "fetching from remote..."
 # 为了避免冲突，强制更新本地文件
 git fetch --all
-git reset --hard origin/master
+git reset --hard origin/main
 echo "done"
 
-#sh pull-code.sh
-#
-#sh build.sh TEST
+sh build.sh TEST
--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 set -x
+
+#work_path=$(dirname $0)
+#cd ./${work_path}
+
+WEB_PATH='/root/code/github.com/'
+cd $WEB_PATH/pymock
 echo "cd to script dir"
-work_path=$(dirname $0)
-cd ./${work_path}
 
 function build()
 {
