e6d3f3d6fc9de25e00b85cc372926b2d940d728c
sampsonliu <sampsonliu@tencent.com>
2021-06-10 19:02:58 +0800

    README

--- build-test.sh
+++ build-test.sh
@@ -1,5 +1,15 @@
 #/bin/sh
 set -x
-sh pull-code.sh
 
-sh build.sh TEST
+echo "start deployment"
+WEB_PATH='/root/code/github.com/'
+cd $WEB_PATH/pymock
+echo "fetching from remote..."
+# 为了避免冲突，强制更新本地文件
+git fetch --all
+git reset --hard origin/master
+echo "done"
+
+#sh pull-code.sh
+#
+#sh build.sh TEST
