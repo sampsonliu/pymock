f10b5383de50771d6828c0059529f4c86e738259
823974765@qq.com <sampsonliu@tencent.com>
2021-06-19 18:06:13 +0800

    更新

--- build.sh
+++ build.sh
@@ -24,6 +24,8 @@ function build()
   && docker-compose -f build.yml build \
   && docker push ccr.ccs.tencentyun.com/sampsonliu/pymock:${version} \
   && echo "build and push finish"
+  && docker images|grep ccr.ccs.tencentyun.com/sampsonliu/pymock|grep none|awk '{print $3}'|xargs docker rmi
+  && echo "docker images|grep ccr.ccs.tencentyun.com/sampsonliu/pymock|grep none|awk '{print $3}'|xargs docker rmi"
 }
 
 case $1 in
